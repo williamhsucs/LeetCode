41df81f0e0e096484eaf2055b1bca69e61e12d58
williamhsucs <williamhsucs@gmail.com>
2021-08-12 15:40:09 +0800

    [TASK] submit problem 977.

---
+++ 977. Squares of a Sorted Array/squares_of_a_sorted_array_william_submit.c
@@ -0,0 +1,55 @@
+/**
+ * Problem
+ * https://leetcode.com/problems/squares-of-a-sorted-array/
+ * 
+ * Input:  [-4,-1,0,3,10]
+ * Output: [0,1,9,16,100]
+ * 
+ * Input:  [-7,-3,2,3,11]
+ * Output: [4,9,9,49,121]
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/**
+ * Time complexity: O(n)
+ * Space complexity: O(n)
+ * 
+ * nums = [-4,-1,0,3,10]   => Compare (-4) and 10
+ *          V         V    => low pointer and high pointer
+ * 
+ * Add the greater one into last place of return array.
+ * retAry = [0,0,0,0,100]  => 10^2 is greater than (-4)^2
+ * 
+ * nums = [-4,-1,0,3,10]   => Move high pointer to next place.
+ *          V      V          Compare (-4) and 3
+ * 
+ * Add the greater one into (last place - 1) of return array.
+ * retAry = [0,0,0,16,100]  => (-4)^2 is greater than 3^2
+ * 
+ * nums = [-4,-1,0,3,10]   => Move low pointer to next place.
+ *             V   V          Compare (-1) and 3
+ */
+int* sortedSquares(int* nums, int numsSize, int* returnSize) {
+    int* retAry = calloc(numsSize, sizeof(int));
+    int poslow = 0;
+    int posHigh = numsSize - 1;
+    int squareLow = 0;
+    int squareHigh = 0;
+    *returnSize = numsSize;
+    
+    while (numsSize-- > 0) {
+        squareLow = nums[poslow] * nums[poslow];
+        squareHigh = nums[posHigh]* nums[posHigh];
+        
+        if (squareLow > squareHigh) {
+            retAry[numsSize] = squareLow;
+            poslow++;
+        } else {
+            retAry[numsSize] = squareHigh;
+            posHigh--;
+        }
+    }
+    
+    return retAry;
+}
