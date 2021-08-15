0a55190f18dc56e036a3f5305e2590cb89566872
williamhsucs <williamhsucs@gmail.com>
2021-08-15 20:50:26 +0800

    [Problem] Update problem 707

--- [Easy] 707. Design Linked List/design_linked_list_author_ dd2233.c
+++ [Easy] 707. Design Linked List/design_linked_list_author_ dd2233.c
@@ -164,15 +164,10 @@ void myLinkedListFree(MyLinkedList* obj) {
  * Your MyLinkedList struct will be instantiated and called as such:
  * MyLinkedList* obj = myLinkedListCreate();
  * int param_1 = myLinkedListGet(obj, index);
- 
  * myLinkedListAddAtHead(obj, val);
- 
  * myLinkedListAddAtTail(obj, val);
- 
  * myLinkedListAddAtIndex(obj, index, val);
- 
  * myLinkedListDeleteAtIndex(obj, index);
- 
  * myLinkedListFree(obj);
 */
 
