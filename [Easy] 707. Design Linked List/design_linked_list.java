86d6147515b32376fcbef0c4647d83dbeabb08a6
williamhsucs <williamhsucs@gmail.com>
2021-08-18 19:46:43 +0800

    [Problem] Update problem 707 comments for complexity

--- [Easy] 707. Design Linked List/design_linked_list.java
+++ [Easy] 707. Design Linked List/design_linked_list.java
@@ -1,3 +1,41 @@
+/**
+ * Problem
+ * https://leetcode.com/problems/design-linked-list/
+ * 
+ * Input:
+ * ["MyLinkedList", "addAtHead", "addAtTail", "addAtIndex", "get", "deleteAtIndex", "get"]
+ * [[], [1], [3], [1, 2], [1], [1], [1]]
+ * 
+ * Output:
+ * [null, null, null, null, 2, null, 3]
+ * 
+ * Explanation
+ * MyLinkedList myLinkedList = new MyLinkedList();
+ * myLinkedList.addAtHead(1);
+ * myLinkedList.addAtTail(3);
+ * myLinkedList.addAtIndex(1, 2);    // linked list becomes 1->2->3
+ * myLinkedList.get(1);              // return 2
+ * myLinkedList.deleteAtIndex(1);    // now the linked list is 1->3
+ * myLinkedList.get(1);              // return 3
+ * 
+ * *************************************************************
+ * Time complexity:
+ *  - Search
+ *      get       -> O(n)
+ *  - Insert
+ *      addAtHead -> O(1)
+ *      addAtTail -> O(n)
+ *      addAtIndex-> O(n)
+ *  - Delete
+ *      delete    -> O(n)
+ * Worst case:
+ * addAtHead only need to use head to add the nodes. O(1)
+ * addAtTail, addAtIndex and delete need to use search to find the index. O(n)
+ * *************************************************************
+ * Space complexity:
+ * O(n)
+ * *************************************************************
+ */
 class MyLinkedList {
   class Node {
     int val;
