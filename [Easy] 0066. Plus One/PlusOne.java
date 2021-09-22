fb804c425b21173150757547c0359865718bf910
williamhsucs <williamhsucs@gmail.com>
2021-09-23 00:33:35 +0800

    [Solution] A better solution for [Easy] 0066. Plus One

--- [Easy] 0066. Plus One/PlusOne.java
+++ [Easy] 0066. Plus One/PlusOne.java
@@ -55,6 +55,43 @@
  */
 class PlusOne {
   public int[] plusOne(int[] digits) {
+    /**
+     * Time Complexity: O(n)
+     */
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] != 9) {
+        digits[i]++;
+        break;
+      } else {
+        digits[i] = 0;
+      }
+    }
+    
+    if (digits[0] == 0) {
+      int[] ret = new int[digits.length + 1];
+      ret[0] = 1;
+      /**
+       * Time Complexity: O(n)
+       * Space Complexity: O(1)
+       */
+      for (int i = 1; i <= ret.length - 1; i++) {
+        ret[i] = digits[i - 1];
+      }
+      
+      return ret;
+    } else {
+      /**
+       * Time Complexity: O(n)
+       * Space Complexity: O(1)
+       */
+      return digits;
+    }
+  }
+}
+
+
+class PlusOneVerOne {
+  public int[] plusOne(int[] digits) {
     int carry = 0;
     
     /**
