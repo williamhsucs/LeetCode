7225c43cd2a5ce855de4625265a7cbc803643611
williamhsucs <williamhsucs@gmail.com>
2021-11-18 00:37:06 +0800

    [Solution] Submit solution for [Easy] 0206. Reverse Linked List

---
+++ [Easy] 0206. Reverse Linked List/ReverseLinkedList.java
@@ -0,0 +1,57 @@
+/**
+ * Problem
+ * https://leetcode.com/problems/reverse-linked-list/
+ * 
+ * ************************************************************* 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * 
+ * Output: [5,4,3,2,1]
+ * 
+ * ************************************************************* 
+ * 
+ * Input: head = [1,2]
+ * 
+ * Output: [2,1]
+ * 
+ * ************************************************************* 
+ * 
+ * Input: head = []
+ * 
+ * Output: []
+ * 
+ * ************************************************************* 
+ * Time Complexity: O()
+ * Space Complexity: O()
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+class ReverseLinkedList {
+  public ListNode reverseList(ListNode head) {
+    ListNode root = new ListNode(0, head);
+    ListNode node = head;
+    ListNode temp = null;
+    
+    // Time Complexity: O(n)
+    while (node.next != null) {
+      temp = node.next.next;
+      node.next.next = root.next;
+      root.next = node.next;
+      node.next = temp;
+    }
+
+    return root.next;
+  }
+}
