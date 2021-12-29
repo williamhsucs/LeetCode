3a09cf2133525dcf7d169e848db68b9d04689b96
williamhsucs <williamhsucs@gmail.com>
2021-12-29 23:36:25 +0800

    [Optimized] Optimized the solution of problem 279.

--- [Medium] 0279. Perfect Squares/PerfectSquares.java
+++ [Medium] 0279. Perfect Squares/PerfectSquares.java
@@ -1,23 +1,38 @@
+/**
+ * Time O(n)
+ * Space (sqrt(n))
+ */
 public class Solution {
   public int numSquares(int n) {
     if (n == 1) return n;
     // Space O(sqrt(n))
     Queue<Integer> queue = new LinkedList<>();
 
+    // Time O(sqrt(n))
     for (int i = (int) Math.floor(Math.sqrt(n)); i > 0; i--) {
       queue.offer(i);
     }
     int minStep = Integer.MAX_VALUE;
 
+    /**
+     * Time O(n)
+     *  Four nest loop for each time cost O(sqrt(n)) should be O(sqrt(n)^4) -> O(n^2)
+     *  However, I used the minStep to break the worst events,
+     *  So, the time complexity will be closely O(n)
+     */
+    // Time O(sqrt(n))
     while (!queue.isEmpty()) {
       int maxSqrtRoot = queue.poll();
+      // Time O(sqrt(n))
       for (int i = maxSqrtRoot; i > 0; i--) {
         int target = n - (maxSqrtRoot * maxSqrtRoot);
         int step = 1;
         int sqrtRoot = i;
+        // Time O(sqrt(n))
         while (sqrtRoot > 0) {
           if (step > minStep) break;
           int sqrt = sqrtRoot * sqrtRoot;
+          // Time O(sqrt(n))
           while (target - sqrt >= 0) {
             target -= sqrt;
             step++;
