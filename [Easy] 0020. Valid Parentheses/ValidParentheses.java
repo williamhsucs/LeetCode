72ce7bf87d99171e6db7031885f4862e36a40e4b
williamhsucs <williamhsucs@gmail.com>
2021-12-30 20:59:51 +0800

    [Solution] Submit solution for [Easy] 0020. Valid Parentheses

---
+++ [Easy] 0020. Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,30 @@
+public class Solution {
+  /**
+   * Time O(n)
+   * Space O(n)
+   */
+  public boolean isValid(String s) {
+    if (s.length() % 2 != 0) return false;
+    // Space O(n)
+    Stack<Character> stack = new Stack<>();
+    // Time O(n)
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      switch (c) {
+        case ')':
+          if (stack.isEmpty() || !stack.pop().equals('(')) return false;
+          break;
+        case ']':
+          if (stack.isEmpty() || !stack.pop().equals('[')) return false;
+          break;
+        case '}':
+          if (stack.isEmpty() || !stack.pop().equals('{')) return false;
+          break;
+        default:
+          stack.push(c);
+          break;
+      }
+    }
+    return stack.isEmpty();
+  }
+}
