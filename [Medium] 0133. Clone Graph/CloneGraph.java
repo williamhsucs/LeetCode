864c0b51add0e7b15674e708f745d07b6c7e2add
williamhsucs <williamhsucs@gmail.com>
2022-01-01 21:28:39 +0800

    [Solution] Submit solution for [Medium] 0133. Clone Graph

---
+++ [Medium] 0133. Clone Graph/CloneGraph.java
@@ -0,0 +1,24 @@
+class Solution {
+  // Space O(n)
+  private Map<Node, Node> visited;
+
+  public Solution() {
+    visited = new HashMap<>();
+  }
+
+  /**
+   * Time O(n)
+   * Space O(h)
+   */
+  public Node cloneGraph(Node node) {
+    if (node == null) return node;
+    if (visited.containsKey(node)) return visited.get(node);
+    Node root = new Node(node.val);
+    visited.put(node, root);
+    for (Node n : node.neighbors) {
+      // Space O(h)
+      root.neighbors.add(cloneGraph(n));
+    }
+    return root;
+  }
+}
