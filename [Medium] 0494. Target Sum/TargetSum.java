26fcfe6fac0cbd83d5f3e78e91d039da7b21dfd3
williamhsucs <williamhsucs@gmail.com>
2022-01-02 21:01:14 +0800

    [Solution] Submit solution for [Medium] 0494. Target Sum

---
+++ [Medium] 0494. Target Sum/TargetSum.java
@@ -0,0 +1,28 @@
+class Solution {
+  /**
+   * Time O(2^n)
+   *   - Process + and - operations.
+   *   - Not a good solution, still can optimize.
+   * Space O(n)
+   */
+  private int dfs(int count, int sum, int[] nums, int target) {
+    int ret = 0;
+    if (count != nums.length) {
+      ret += dfs(count + 1, sum + nums[count], nums, target);
+      ret += dfs(count + 1, sum - nums[count], nums, target);
+      return ret;
+    }
+
+    if (sum == target) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  public int findTargetSumWays(int[] nums, int target) {
+    int count = 0;
+    int sum = 0;
+    return dfs(count, sum, nums, target);
+  }
+}
