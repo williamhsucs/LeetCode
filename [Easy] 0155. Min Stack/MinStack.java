9557eb99bebe08c44b6073735b486c81ff7402a3
williamhsucs <williamhsucs@gmail.com>
2022-01-11 23:24:54 +0800

    [Optimized] Optimized the solution of problem 155

--- [Easy] 0155. Min Stack/MinStack.java
+++ [Easy] 0155. Min Stack/MinStack.java
@@ -1,12 +1,59 @@
 class MinStack {
+  private class Node {
+    int min;
+    int val;
+    Node prev;
+    
+    public Node(int val, int min, Node node) {
+      this.val = val;
+      this.min = min;
+      this.prev = node;
+    }
+  }
+  
+  private Node tail;
+  
+  // Time O(1)
+  public int getMin() {
+    return tail.min;
+  }
+  
+  // Time O(1)
+  public int top() {
+    return tail.val;
+  }
+  
+  /**
+   * Time O(1)
+   * Space O(1)
+   */ 
+  public void push(int val) {
+    Node cur;
+    if (tail == null) {
+      cur = new Node(val, val, tail);
+    } else {
+      cur = new Node(val, Math.min(tail.min, val), tail);
+    }
+    tail = cur;
+  }
+  
+  // Time O(1)
+  public void pop() {
+    tail = tail.prev;
+  }
+}
+
+class MinStack2 {
 
   private int min;
   private List<Integer> stack;
 
+  // Space O(n)
   public MinStack() {
     stack = new ArrayList<>();
   }
   
+  // Time O(1)
   public void push(int val) {
     if (stack.isEmpty()) {
       min = val;
@@ -16,6 +63,7 @@ class MinStack {
     stack.add(val);
   }
   
+  // Time O(n)
   public void pop() {
     int i = stack.get(stack.size() - 1);
     stack.remove(stack.size() - 1);
@@ -25,12 +73,15 @@ class MinStack {
         min = j < min ? j : min;
       }
     }
+    
   }
   
+  // Time O(1)
   public int top() {
     return stack.get(stack.size() - 1);
   }
   
+  // Time O(1)
   public int getMin() {
     return min;
   }
