e62681087cbdc750f794ea8847162ece29608d0d
williamhsucs <williamhsucs@gmail.com>
2022-01-16 16:32:34 +0800

    [Solution] Submit solution for [Medium] 0139. Word Break

---
+++ [Medium] 0139. Word Break/WordBreak.java
@@ -0,0 +1,21 @@
+class Solution {
+  private Set<String> wrongRecord = new HashSet<>();
+
+  public boolean wordBreak(String target, List<String> wordDict) {
+    if (target == null || target.length() == 0) return false;
+    return dfs(target, wordDict);
+  }
+
+  public boolean dfs(String target, List<String> wordDict) {
+    if (target.length() == 0) return true;
+    if (wrongRecord.contains(target)) return false;
+    for (String word : wordDict) {
+      if (target.startsWith(word)) {
+        if (dfs(target.substring(word.length()), wordDict)) return true;
+      }
+    }
+    wrongRecord.add(target);
+    return false;
+  }
+
+}
