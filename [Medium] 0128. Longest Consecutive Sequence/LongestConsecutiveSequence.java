c97b7d3f738f55789c1b6832aff53f3d73cb4989
williamhsucs <williamhsucs@gmail.com>
2022-01-23 23:56:40 +0800

    [Solution] Submit solution for [Medium] 0128. Longest Consecutive Sequence

---
+++ [Medium] 0128. Longest Consecutive Sequence/LongestConsecutiveSequence.java
@@ -0,0 +1,34 @@
+class Solution {
+  /**
+   * Time O(n)
+   * Space O(n)
+   */
+  public int longestConsecutive(int[] nums) {
+    // Space O(n)
+    Set<Integer> set = new HashSet<>();
+    int max = 0;
+    
+    // Time O(n)
+    for (int i : nums) set.add(i);
+    
+    // Time O(n)
+    for (int i : nums) {
+      if (set.contains(i)) {
+        set.remove(i);
+        int left = i;
+        int right = i;
+        while (set.contains(left - 1)) {
+          set.remove(left);
+          left--;
+        }
+        while (set.contains(right + 1)) {
+          set.remove(right);
+          right++;
+        }
+        max = Math.max(max , right - left + 1);
+      }
+    }
+    
+    return max;
+  }
+}
