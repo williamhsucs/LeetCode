08c5bcbaf6b371b4f3184ff90269981ee166160f
williamhsucs <williamhsucs@gmail.com>
2022-02-07 23:51:25 +0800

    [Optimized] Time Complexity O(nlogn) -> O(n)

--- [Medium] 0347. Top K Frequent Elements/TopKFreqE.java
+++ [Medium] 0347. Top K Frequent Elements/TopKFreqE.java
@@ -1,92 +1,40 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Problem
- * https://leetcode.com/problems/top-k-frequent-elements/
- * 
- * ************************************************************* 
- * Time Complexity: O(nlogn)
- * Space Complexity: O(n)
- */
 class TopKFreqE {
+  /**
+   * Time O(n)
+   * Space O(n)
+   */
   public int[] topKFrequent(int[] nums, int k) {
     // Space O(n)
-    Map<Integer, Integer> map = new HashMap<>();
-    // Space O(n)
-    Map<Integer, List<Integer>> tree = new TreeMap<>(Collections.reverseOrder());
-    // Space O(n)
-    List<Integer> aryList = new ArrayList<>();
-    // Space O(k)
-    int[] ret = new int[k];
-    int count = 0;
-    
+    Map<Integer, Integer> count = new HashMap<>();
+    Map<Integer, List<Integer>> bucket = new HashMap<>();
+    int max = 0;
     // Time O(n)
     for (int i : nums) {
-      // Time O(1)
-      map.put(i, map.getOrDefault(i, 0) + 1);
+      count.put(i, count.getOrDefault(i, 0) + 1);
+      max = Math.max(max, count.get(i));
     }
-    
-    /**
-     * Time O(nlogn)
-     */
     // Time O(n)
-    for (int key : map.keySet()) {
-      int treeKey = map.get(key);
-      // Time O(logn)
-      aryList = tree.getOrDefault(treeKey, new ArrayList<>());
-      // Time O(1)
-      aryList.add(key);
-      // Time O(logn)
-      tree.put(treeKey, aryList);
+    for (int key : count.keySet()) {
+      List<Integer> list = new ArrayList<>();
+      if (bucket.containsKey(count.get(key))) {
+        list = bucket.get(count.get(key));
+      }
+      list.add(key);
+      bucket.put(count.get(key), list);
     }
-
-    // Time O(k), count == k will return
-    for (List<Integer> list : tree.values()) {
-      for (int i : list) {
-        if (count == k) break;
-        ret[count++] = i;
+    int[] res = new int[k];
+    int resCount = 0;
+    // Time O(k)
+    for (int i = max; i > 0; i--) {
+      List<Integer> list = bucket.get(i);
+      if (list != null) {
+        for (int j = 0; j < list.size(); j++) {
+          res[resCount++] = list.get(j);
+          k--;
+          if (k == 0) return res;
+        }
       }
     }
-
-    return ret;
-  }
-}
-
-
-
-
-/**
- * Wonderful Solution
- * Author: mo10
- */
-
-class TopKFreqEmo10 {
-  public List<Integer> topKFrequent(int[] nums, int k) {
-
-    List<Integer>[] bucket = new List[nums.length + 1];
-    Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
-
-    for (int n : nums) {
-      frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
-    }
-
-    for (int key : frequencyMap.keySet()) {
-      int frequency = frequencyMap.get(key);
-      if (bucket[frequency] == null) {
-        bucket[frequency] = new ArrayList<>();
-      }
-      bucket[frequency].add(key);
-    }
-
-    List<Integer> res = new ArrayList<>();
-
-    for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
-      if (bucket[pos] != null) {
-        res.addAll(bucket[pos]);
-      }
-    }
-    return res;
+    return null;
   }
 }
