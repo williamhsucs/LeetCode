54071318732aee503aeee0a47a41e808ebaa032e
williamhsucs <williamhsucs@gmail.com>
2022-03-26 23:39:42 +0800

    [Update] Add new solutions.

--- [Easy] 0119. Pascal's Triangle II/PascalTriangleII.java
+++ [Easy] 0119. Pascal's Triangle II/PascalTriangleII.java
@@ -1,4 +1,23 @@
-class PascalTriangleII {
+class PascalTriangleIIMath {
+  /**
+   * Time O(n)
+   * Space O(n)
+   */
+  public List<Integer> getRow(int rowIndex) {
+    List<Integer> row = new ArrayList<>(){
+      {
+        add(1);
+      }
+    };
+    // Time O(n)
+    for (int i = 1; i <= rowIndex; i++) {
+      row.add((int) ((long) row.get(i - 1) * (long) (rowIndex - i + 1) / (long) i));
+    }
+    return row;
+  }
+}
+
+class PascalTriangleIIDP {
   /**
    * Time O(mn)
    * Space O(rowIndex)
